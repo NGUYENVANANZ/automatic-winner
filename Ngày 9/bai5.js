20c78381eb5965291bc29dcf3685b9de1c1ff332
Nguyễn Văn An <vanankv123@gmail.com>
2022-08-10 17:14:36 +0700

    add

--- Ngày 9/bai5.js
+++ Ngày 9/bai5.js
@@ -1,45 +1,14 @@
-let so = "0"
-let KQ = 0
-let tinh = "+"
 function Nhannut(phimbam) {
-    if (parseInt(phimbam) == phimbam) {
-        so += phimbam
-        document.getElementById('bieu_thuc').innerHTML += phimbam
-    } else
         if (phimbam == "C") {
-            LamMoi()
+            document.getElementById('bieu_thuc').innerHTML = ""
+            document.getElementById('ket_qua').innerHTML = ""
         } else
             if (phimbam == "=") {
-                BaiToan()
-                tinh = "="
-                document.getElementById('ket_qua').innerHTML = KQ.toString()
+                let bieuThuc = document.getElementById('bieu_thuc').innerHTML
+                document.getElementById('ket_qua').innerHTML = eval(bieuThuc)
             } else {
                 document.getElementById('bieu_thuc').innerHTML += phimbam
-                BaiToan()
-                tinh = phimbam
             }
 }
-function LamMoi() {
-    document.getElementById('bieu_thuc').innerHTML = ""
-    document.getElementById('ket_qua').innerHTML = 0
-    so = "0"
-    KQ = 0
-    tinh = "+"
-}
-function BaiToan(){
-    if (tinh ===  "+") {
-        KQ += parseFloat(so)
-    }
-    if (tinh === "-") {
-        KQ -= parseFloat(so)
-    }
-    if (tinh === "*") {
-        KQ *= parseFloat(so)
-    }
-    if (tinh === "/") {
-        KQ /= parseFloat(so)
-    }
-    so = "0"
 
-}
 
